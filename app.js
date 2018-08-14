06a3a195c983d36addf09175af8fd0b16ecc0d1c
MT Rojas <tererojas.i@gmail.com>
2018-08-14 03:16:33 +0000

    Cleaned code in app.js

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ var campgroundRoutes = require('./routes/campgrounds'),
     commentRoutes    = require('./routes/comments'),
     indexRoutes      = require('./routes/index');
 
-console.log(process.env.DB);
+
 mongoose.connect(process.env.DB, { useNewUrlParser: true });   
-//mongoose.connect('mongodb://MTRojas:12Arandano@ds163630.mlab.com:63630/yelpcamp', { useNewUrlParser: true }); 
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));

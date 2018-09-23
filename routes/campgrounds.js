23901daf119327acc3e996b412f7d028f916b0cc
MT Rojas <tererojas.i@gmail.com>
2018-09-23 22:54:58 +0000

    Improve UI: Login, Signup, Nav-bar and registration error message

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -13,7 +13,7 @@ router.get('/', function(req, res) {
     if(err) {
       console.log(err); 
     } else {
-      res.render('campgrounds/index', {campgrounds: allCampgrounds});
+      res.render('campgrounds/index', {campgrounds: allCampgrounds, page: 'campgrounds'});
     }
   });
 });

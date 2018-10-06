9561d8975a231ae0ddc2a3b6963d8dc0b9454278
MT Rojas <tererojas.i@gmail.com>
2018-10-06 21:53:29 +0000

    Create user profile view, modify user model and register view. Add user profile routes to index.js.

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var User       = require('../models/user');
+var Campground = require('../models/campground');
 var passport   = require('passport');
 var express    = require('express');
 var router     = express.Router();
@@ -16,7 +17,13 @@ router.get('/register', function(req, res) {
 
 //handle signup logic
 router.post('/register', function(req, res) {
-  var newUser = new User({username: req.body.username});
+  var newUser = new User({
+    username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    avatar: req.body.avatar,
+    email: req.body.email
+  });
   //locus is a npm package that allows us to stop the code whenever 
   //it hits this point in the route and then we can take a look at whats going on, we can see the variables available to us
   //eval(require('locus')); 
@@ -32,7 +39,7 @@ router.post('/register', function(req, res) {
       req.flash('success', 'Welcome to YelpCamp ' + user.username);
       res.redirect('/campgrounds');
     });
-  });
+  }); 
 });
 
 //show login form
@@ -54,6 +61,24 @@ router.get('/logout', function(req, res) {
   req.logout();
   req.flash('success', 'Logged you out!');
   res.redirect('/campgrounds');
+});
+
+//user profile
+router.get('/user/:id', function(req, res) {
+  User.findById(req.params.id, function(err, foundUser) {
+    if(err) {
+      req.flash('error', 'Something went wrong.');
+      return res.redirect('/');
+    }
+    Campground.find().where('author.id').equals(foundUser._id).exec(function(err, campgrounds){
+      if(err) {
+        req.flash('error', 'Something went wrong.');
+        return res.redirect('/'); 
+      }
+      //eval(require('locus'));
+      res.render('user/show', {user: foundUser, campgrounds: campgrounds});
+    });
+  });
 });
 
 

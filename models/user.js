c1e112fd68984a9b329d05948aaca1404122b80c
MT Rojas <tererojas.i@gmail.com>
2018-10-08 19:12:08 +0000

    Created forgot password routes and views.

--- models/user.js
+++ models/user.js
@@ -2,12 +2,22 @@ var passportLocalMongoose = require('passport-local-mongoose'),
     mongoose              = require('mongoose');
 
 var userSchema = new mongoose.Schema({
-  name: String,
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
   password: String,
   avatar: String,
   firstName: String,
   lastName: String,
-  email: String,
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
   isAdmin: {
     type: Boolean,
     default: false
